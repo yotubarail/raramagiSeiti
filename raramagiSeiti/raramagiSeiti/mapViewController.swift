c4cb0b1686c54b6ca33427da505f63241d14e094
yotubarail <47407052+yotubarail@users.noreply.github.com>
2020-06-23 16:58:56 +0900

    Add files via upload

---
+++ raramagiSeiti/raramagiSeiti/mapViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ViewController.swift
+//  raramagiSeiti
+//
+//  Created by 滝浪翔太 on 2020/06/19.
+//  Copyright © 2020 滝浪翔太. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class mapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 位置情報使用の通知を表示
+        locationManager.requestAlwaysAuthorization()
+        
+        // 地図の真ん中を決める
+        let centerLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(35.747744, 139.429784)
+        mapView.setCenter(centerLocation, animated: true)
+        
+        // 緯度と経度から縮尺を決める
+        var region:MKCoordinateRegion = mapView.region
+            region.center = centerLocation
+            region.span.latitudeDelta = 0.02
+            region.span.longitudeDelta = 0.03
+        mapView.setRegion(region,animated:true)
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+        
+        // 東大和市内
+        // ピンを生成
+        let higasiyamatosiStaPin = MKPointAnnotation()
+        let botanicaiGardenPin = MKPointAnnotation()
+        let bigboxPin = MKPointAnnotation()
+        let electricalPin = MKPointAnnotation()
+        let cityHallPin = MKPointAnnotation()
+        let kamikitadaiStaPin = MKPointAnnotation()
+        let museumPin = MKPointAnnotation()
+        let shrinePin = MKPointAnnotation()
+        let tamakoPin = MKPointAnnotation()
+        
+        // ピンの座標設定
+        higasiyamatosiStaPin.coordinate = CLLocationCoordinate2DMake(35.733017, 139.434828)
+        botanicaiGardenPin.coordinate = CLLocationCoordinate2DMake(35.732407, 139.434765)
+        bigboxPin.coordinate = CLLocationCoordinate2DMake(35.733484, 139.434225)
+        electricalPin.coordinate = CLLocationCoordinate2DMake(35.735214, 139.421357)
+        cityHallPin.coordinate = CLLocationCoordinate2DMake(35.745183, 139.426403)
+        kamikitadaiStaPin.coordinate = CLLocationCoordinate2DMake(35.745847, 139.415895)
+        museumPin.coordinate = CLLocationCoordinate2DMake(35.754921, 139.423374)
+        shrinePin.coordinate = CLLocationCoordinate2DMake(35.765060, 139.441964)
+        tamakoPin.coordinate = CLLocationCoordinate2DMake(35.759907, 139.439490)
+        
+        // タイトルを設定
+        higasiyamatosiStaPin.title = "東大和市駅"
+        botanicaiGardenPin.title = "東京都薬用植物園"
+        bigboxPin.title = "BIGBOX東大和"
+        electricalPin.title = "旧日立航空機立川工場変電所"
+        cityHallPin.title = "東大和市役所"
+        kamikitadaiStaPin.title = "上北台駅"
+        museumPin.title = "東大和市立郷土博物館"
+        shrinePin.title = "氷川神社"
+        tamakoPin.title = "多摩湖"
+
+        // mapViewにピンを追加
+        mapView.addAnnotation(higasiyamatosiStaPin)
+        mapView.addAnnotation(botanicaiGardenPin)
+        mapView.addAnnotation(bigboxPin)
+        mapView.addAnnotation(electricalPin)
+        mapView.addAnnotation(cityHallPin)
+        mapView.addAnnotation(kamikitadaiStaPin)
+        mapView.addAnnotation(museumPin)
+        mapView.addAnnotation(shrinePin)
+        mapView.addAnnotation(tamakoPin)
+        
+        
+        // その他の地域
+        let shoppingStreetPin = MKPointAnnotation()
+        
+        shoppingStreetPin.coordinate = CLLocationCoordinate2DMake(35.661186, 139.667618)
+        
+        shoppingStreetPin.title = "下北沢南商店街"
+        
+        mapView.addAnnotation(shoppingStreetPin)
+        
+    }
+
+
+}
+
---
+++ raramagiSeiti/raramagiSeiti/seitiViewController.swift
@@ -0,0 +1,136 @@
+//
+//  seitiViewController.swift
+//  raramagiSeiti
+//
+//  Created by 滝浪翔太 on 2020/06/19.
+//  Copyright © 2020 滝浪翔太. All rights reserved.
+//
+
+import UIKit
+
+class seitiViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var listTableView: UITableView!
+    
+    let higasiyamatoSeitiList: NSMutableArray = ["東大和市駅", "東京都薬用植物園", "BIGBOX東大和", "旧日立航空機立川工場変電所", "東大和市役所", "上北台駅", "東大和市立郷土博物館", "氷川神社", "多摩湖（村山貯水池）"]
+    let otherSeitiList: NSMutableArray = ["下北沢南商店街"]
+    
+    var listImg: Array<UIImage> = []
+
+    var section1: Dictionary = [String: NSMutableArray]()
+    var section2: Dictionary = [String: NSMutableArray]()
+    var sections: Array = [Dictionary<String,NSMutableArray>]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        section1 = ["東大和市": higasiyamatoSeitiList]
+        section2 = ["その他": otherSeitiList]
+        
+        sections.append(section1)
+        sections.append(section2)
+        
+        listTableView.delegate = self
+        listTableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var title = ""
+        for (key) in sections[section].keys
+        {
+            title = key
+        }
+        return title
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // テーブルビューのセル数の設定する。
+        switch section {
+        case 0:
+            return self.higasiyamatoSeitiList.count
+        case 1:
+            return self.otherSeitiList.count
+        default: return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "seitiCell", for:indexPath as IndexPath)
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+        for (value) in sections[indexPath.section].values
+        {
+         cell.textLabel?.text = value[indexPath.row] as? String
+         cell.textLabel?.font = UIFont.systemFont(ofSize:20)
+        }
+        
+        cell.imageView?.image = UIImage(named: "seiti\(indexPath.section)\(indexPath.row).png")
+        
+        return cell
+    }
+    
+    
+    // それぞれのcellをタップしたときの処理
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        switch indexPath.section {
+            
+        // 東大和市聖地
+        case 0: switch indexPath.row {
+            
+            case 0: tableView.deselectRow(at: indexPath, animated: true)
+            print("東大和市駅をタップ")
+            
+            case 1: tableView.deselectRow(at: indexPath, animated: true)
+            print("東京都薬用植物園をタップ")
+            
+            case 2: tableView.deselectRow(at: indexPath, animated: true)
+            print("BIGBOX東大和をタップ")
+            
+            case 3: tableView.deselectRow(at: indexPath, animated: true)
+            print("旧日立航空機立川工場変電所をタップ")
+            
+            case 4: tableView.deselectRow(at: indexPath, animated: true)
+            print("東大和市役所をタップ")
+            
+            case 5: tableView.deselectRow(at: indexPath, animated: true)
+            print("上北台駅をタップ")
+            
+            case 6: tableView.deselectRow(at: indexPath, animated: true)
+            print("東大和市立郷土博物館をタップ")
+            
+            case 7: tableView.deselectRow(at: indexPath, animated: true)
+            print("氷川神社をタップ")
+            
+            case 8: tableView.deselectRow(at: indexPath, animated: true)
+            print("多摩湖（村山貯水池）をタップ")
+            
+            default: break
+            }
+            
+            
+        // その他の地域の聖地
+        case 1: switch indexPath.row {
+            
+            case 0: tableView.deselectRow(at: indexPath, animated: true)
+            print("下北沢南商店街をタップ")
+            
+            case 1: tableView.deselectRow(at: indexPath, animated: true)
+
+            default: break
+            }
+         
+        default: break
+        }
+    }
+    
+    // UITableViewの高さを指定
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(55)
+    }
+
+}
